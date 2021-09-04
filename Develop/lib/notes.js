b0494cece6bf166860d8b243abaa45369f84ba43
Mike Veneziale <falciformdesign@gmail.com>
2021-09-04 10:37:59 -0500

    posts json data and writes it to new file and adds to notesarray

--- Develop/lib/notes.js
+++ Develop/lib/notes.js
@@ -3,8 +3,13 @@ const path = require('path');
 
 
 function createNewNote(body, notesArray) {
-  console.log(body);
-  return body;
+  const note = body;
+  notesArray.push(note);
+  fs.writeFileSync(
+    path.join(__dirname, '../db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+  )
+  return note;
  }
 
 
--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
-
+const { createNewNote } = require('../lib/notes')
 // const index = require('../public/assets/js/index.js');
-const { db } = require('../db/db.json');
+const { notes } = require('../db/db.json');
 
 
 router.get('/api/notes', (req, res) => {
@@ -10,21 +10,11 @@ router.get('/api/notes', (req, res) => {
 })
 
 
-// function createNewNote(body, notesArray) {
-//   const note = body;
-//   notesArray.push(note);
-//   false.writeFileSync(
-//     path.join(__dirname, '../db/db.json'),
-//     JSON.stringify({ notesArray }, null, 2)
-//   );
-//   return note;
-// }
-
 router.post('/api/notes', (req, res) => {
-  console.log(req.body);
-  res.json(req.body);
-  // const note = createNewNote(req.body, db);
-  // res.json(note);
+  // console.log(req.body);
+  // res.json(req.body);
+  const note = createNewNote(req.body, notes);
+  res.json(note);
 })
 
 
